123107caecf56d71559794c104ee9b39f568dd77
BoringMan60101 <80347868+BoringMan60101@users.noreply.github.com>
2022-04-10 20:57:28 +0500

    Add files via upload

---
+++ lab4/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        int code = 0;
+        void pisatel1(object mut)
+        {
+            Mutex mutex = mut as Mutex; 
+            mutex.WaitOne();
+            Random rand = new Random();
+            int kol, i, j;
+            string[] frukti = { "яблоко", "апельсин", "ананас", "банан", "груша", "авокадо", "лимон"};
+            string vivod = "";
+            kol = 1 + rand.Next() % 6;
+            for (i = 0; i < kol; i++)
+            {
+                j = rand.Next() % 7;
+                vivod = vivod + frukti[j] + " ";
+            }
+            textBox5.Text = vivod;
+            code = 1;
+            textBox1.Text = vivod;
+            textBox2.Text = "";
+            Thread.Sleep(2000);
+            mutex.ReleaseMutex();
+        }
+
+        void pisatel2(object mut)
+        {
+            Mutex mutex = mut as Mutex;
+            while (code != 2)
+                Thread.Sleep(1000);
+            mutex.WaitOne();
+            Random rand = new Random();
+            int kol, i, j;
+            string[] mebel = { "стол", "стул", "шкаф", "кровать", "табуретка", "кресло", "комод"};
+            string vivod = "";
+            kol = 1 + rand.Next() % 6;
+            for (i = 0; i < kol; i++)
+            {
+                j = rand.Next() % 7;
+                vivod = vivod + mebel[j] + " ";
+            }
+            textBox5.Text = vivod;
+            textBox2.Text = "";
+            code = 3;
+            textBox2.Text = vivod;
+            Thread.Sleep(2000);
+            mutex.ReleaseMutex();
+        }
+
+        void chitatel1(object mut)
+        {
+            while(code != 1)
+                Thread.Sleep(1000);
+            Mutex mutex = mut as Mutex;
+            mutex.WaitOne();
+            string[] slova = textBox5.Text.Split(); 
+            int kol_sl_s_A = 0;
+            for (int i = 0; i < slova.Length-1; i++)
+            {
+                if(slova[i][0] == 'а')
+                    kol_sl_s_A++;
+            }
+            textBox5.Text = "";
+            textBox3.Text = "Kolichestvo slov nachinayushixsya s bukvi A v obshem pole ravno = " + kol_sl_s_A.ToString();
+            code = 2;
+            Thread.Sleep(2000);
+            mutex.ReleaseMutex();
+        }
+
+        void chitatel2(object mut)
+        {
+            Mutex mutex = mut as Mutex;
+            while(code != 3)
+                Thread.Sleep(1000);
+            mutex.WaitOne();
+            string[] slova = textBox5.Text.Split();
+            int kol_slov = slova.Length-1;
+            textBox5.Text = "";
+            textBox4.Text = "Kolichestvo slov v obshem pole ravno = " + kol_slov.ToString();
+            code = 4;
+            Thread.Sleep(2000);
+            mutex.ReleaseMutex();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Mutex mutex = new Mutex();
+            Thread pis1 = new Thread(pisatel1);
+            Thread pis2 = new Thread(pisatel2);
+            Thread chit1 = new Thread(chitatel1);
+            Thread chit2 = new Thread(chitatel2);
+            pis1.Start(mutex);
+            chit1.Start(mutex);
+            pis2.Start(mutex);
+            chit2.Start(mutex);
+        }
+    }
+}
---
+++ lab5_simply/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace lab5
+{ 
+    public partial class Form1 : Form
+    {
+
+        string put = "C:/"; //сначала отображаем содержимое корневого каталога
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //обработка кнопки "назад"
+        {
+            //По нажатию кнопки "назад" отображается содержимое каталога (папки) предыдущей (на уровень выше)
+            listBox1.Items.Clear(); //освобождение поля от предыдущей записи
+            DirectoryInfo pred_dir = Directory.GetParent(put); //получение объекта - родительского каталога
+            if (pred_dir == null) //проверка, если родителя уже нет (самый высокий уровень)
+            {
+                put = "C:/"; //мы фактически и так в этом каталоге, но запишем его ещё раз
+                MessageBox.Show("достигнут корневой каталог (выше некуда)"); //вывод соответсвующего сообщения
+            }
+            else
+                put = pred_dir.FullName; //если есть родительский каталог (на уровень выше)
+
+            string[] papki = Directory.GetDirectories(put); //получение папок в данном каталоге
+            foreach (string dir in papki) //вывод папок
+                listBox1.Items.Add(dir);
+            string[] files = Directory.GetFiles(put); //получение файлов (их имён)
+            foreach (string dir in files) //вывод
+                listBox1.Items.Add(dir);
+        }
+
+        private void button2_Click(object sender, EventArgs e) //обработка кнопки "отобразить"
+        {
+            //Отображает содержимое введённо вручную каталога (из TextBox)
+            put = textBox1.Text; //получили введёный вручную путь
+            //if {} else - обработка ошибочного и существующего пути
+            if(Directory.Exists(put) == false)
+            {
+                MessageBox.Show("введён неправильный путь к папке");
+                return;
+            }
+            try
+            {
+                listBox1.Items.Clear(); //освобождение поля от предыдущей записи
+                //ниже вывод сначала папок, потом каталогов выбранного каталога
+                string[] papki = Directory.GetDirectories(put); 
+                foreach (string dir in papki)
+                    listBox1.Items.Add(dir);
+                string[] files = Directory.GetFiles(put);
+                foreach (string dir in files)
+                    listBox1.Items.Add(dir);
+            }
+            catch 
+            { MessageBox.Show("ошибка при открытии папки"); }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //этот код запустится сразу после запуска программы
+        {
+            //при запуске программы сразу выводится содержимое корневого каталога (диск С)
+            string[] papki = Directory.GetDirectories(put);
+            foreach (string dir in papki)
+                listBox1.Items.Add(dir);
+            string[] files = Directory.GetFiles(put);
+            foreach (string dir in files)
+                listBox1.Items.Add(dir);
+        }
+    }
+}
